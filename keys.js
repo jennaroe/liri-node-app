e0a8acd8f9ded85285b503c331910903527c0d17
Jennifer Rose Hernandez <jennrosedev@gmail.com>
2016-06-18 12:14:54 -0400

    Delete keys.js

--- keys.js
+++
@@ -1,7 +0,0 @@
-
-exports.twitterKeys = {
-  consumer_key: 'KZ84dTH5W8iqYiZdmIZthIdMr',
-  consumer_secret: 'XBGlRCwVSxqnjH51OAQPYXv6Yb02qp1eQNdgn6dHrxYcgdX9Of',
-  access_token_key: '27723748-NAtNYeDrvmhRTZBQx9e79vyzEpMo9odfu5S9qCA31',
-  access_token_secret: 'WHhVTf17KYzyrfe4fTytlwmhE46yajYUhElTlcg1y1TnG',
-}
